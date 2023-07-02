53758d2bb233d8969eeb30ec04ac098b48ef449c
wzaprawa <wzaprawa@gmail.com>
2023-07-02 15:55:55 +0200

    Added top bar layout

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,7 +1,23 @@
 import "./HomePage.scss";
 
 function HomePage() {
-  return <div className="home-page-wrapper"></div>;
+  return (
+    <div className="home-page-wrapper">
+      <div className="top-bar">
+        <div className="top-bar-left-side">
+          <div className="top-bar-left-side-upper">
+            <span>Plony</span>
+          </div>
+          <div className="top-bar-left-side-lower">
+            <span>Kontroluj stan zebranych plonów</span>
+          </div>
+        </div>
+        <div className="top-bar-right-side">
+          <div className="primary-button">+ Dodaj</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
