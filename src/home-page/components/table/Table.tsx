f2a0ec97289db64220665ef51102fc7d2182ef4f
wzaprawa <wzaprawa@gmail.com>
2023-07-06 19:17:06 +0200

    Working on table styles

--- src/home-page/components/table/Table.tsx
+++ src/home-page/components/table/Table.tsx
@@ -1,7 +1,30 @@
 import "./Table.scss";
 
 function Table() {
-  return <div className="table-container"></div>;
+  return (
+    <div className="table-container">
+      <table>
+        <tr className="header-row">
+          <th>Nazwa</th>
+          <th>Odmiana</th>
+          <th>Ilość</th>
+          <th>Data zbioru</th>
+          <th>Stan</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>
+            Truskawka <p>Słodka odmiana polecana</p>
+          </td>
+          <td>Rumba</td>
+          <td>20kg</td>
+          <td>26/06/2023</td>
+          <td>Normalny</td>
+          <td>🗑️✏️</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default Table;
