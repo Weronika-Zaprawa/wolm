637511d0a9ce3f4ca8e43225528c8ce3ee80c126
wzaprawa <wzaprawa@gmail.com>
2023-09-11 18:52:13 +0200

    Added form submit handling

--- src/home-page/components/harvest-form/HarvestForm.tsx
+++ src/home-page/components/harvest-form/HarvestForm.tsx
@@ -1,19 +1,65 @@
-import { RefObject } from "react";
+import { FormEvent, RefObject, useRef } from "react";
 import { useState } from "react";
 import "./HarvestForm.scss";
 
-function HarvestForm() {
+type HarvestFormValues = {
+  name: string;
+  date: string;
+  variety: string;
+  state: string;
+  category: string;
+  amount: string;
+  unit: string;
+  information: string;
+};
+
+type HarvestFormProps = {
+  onSubmit: (values: HarvestFormValues) => void;
+  formRef: RefObject<HTMLFormElement>;
+};
+
+function HarvestForm({ onSubmit, formRef }: HarvestFormProps) {
   const [name, setName] = useState<string>("");
+  const [date, setDate] = useState<string>("");
+  const [variety, setVariety] = useState<string>("");
+  const [state, setState] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [amount, setAmount] = useState<string>("");
+  const [unit, setUnit] = useState<string>("");
+  const [information, setInformation] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit({
+      name,
+      date,
+      variety,
+      state,
+      category,
+      amount,
+      unit,
+      information,
+    });
+  };
+
   return (
-    <form>
+    <form ref={formRef} onSubmit={handleSubmit}>
       <div className="row">
         <div className="cell large">
           <label htmlFor="name">Nazwa</label>
-          <input id="name" type="text" placeholder="Nazwa"></input>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            type="text"
+            placeholder="Nazwa"
+          ></input>
         </div>
         <div className="cell small">
           <label htmlFor="date">Data</label>
           <input
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
             type="text"
             placeholder="MM/DD/YYYY"
             onFocus={(e) => (e.target.type = "date")}
@@ -24,11 +70,21 @@ function HarvestForm() {
       <div className="row">
         <div className="cell large">
           <label htmlFor="variety">Odmiana</label>
-          <input id="variety" type="text" placeholder="Odmiana"></input>
+          <input
+            value={variety}
+            onChange={(e) => setVariety(e.target.value)}
+            id="variety"
+            type="text"
+            placeholder="Odmiana"
+          ></input>
         </div>
         <div className="cell small">
           <label htmlFor="state">Stan</label>
-          <select id="state">
+          <select
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+            id="state"
+          >
             <option value="" disabled selected>
               Stan
             </option>
@@ -39,7 +95,11 @@ function HarvestForm() {
       <div className="row medium">
         <div className="cell medium">
           <label htmlFor="category">Kategoria</label>
-          <select id="category">
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            id="category"
+          >
             <option value="" disabled selected>
               Kategoria
             </option>
@@ -49,6 +109,8 @@ function HarvestForm() {
         <div className="cell small">
           <label htmlFor="amount">Ilość</label>
           <input
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
             id="amount"
             type="number"
             placeholder="10"
@@ -58,7 +120,11 @@ function HarvestForm() {
         </div>
         <div className="cell extra-small">
           <label htmlFor="unit">Jednostka</label>
-          <select id="unit">
+          <select
+            value={unit}
+            onChange={(e) => setUnit(e.target.value)}
+            id="unit"
+          >
             <option value="kg" selected>
               kg
             </option>
@@ -70,6 +136,8 @@ function HarvestForm() {
         <div className="cell extra-large">
           <label htmlFor="information">Informacje</label>
           <input
+            value={information}
+            onChange={(e) => setInformation(e.target.value)}
             id="information"
             type="text"
             placeholder="Dodatkowe informacje"
